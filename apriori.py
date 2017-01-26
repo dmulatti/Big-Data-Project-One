c8bfea8ca94a1630b4d2bf014434835cb4d9bb95
David Mulatti <david@mulatti.com>
2017-01-26 17:49:40 -0500

    Add user choice for support threshold and update project description

--- apriori.py
+++ apriori.py
@@ -13,7 +13,7 @@ def get_most_frequent_items(in_file, lines_to_process, threshold):
 		for product in basket:
 			cnt[product] += 1
 
-	return [k for k,v in dict(cnt).items() if v >= threshold]
+	return [k for k,v in cnt.items() if v >= threshold]
 
 
 def get_most_frequent_pairs(in_file, lines_to_process, item_pairs, threshold):
@@ -26,31 +26,36 @@ def get_most_frequent_pairs(in_file, lines_to_process, item_pairs, threshold):
 			if pair[0] in basket and pair[1] in basket:
 				cnt[pair] += 1
 
-	return [k for k,v in dict(cnt).items() if v >= threshold]
+	return [k for k,v in cnt.items() if v >= threshold]
 
 
 def main():
+	if len(sys.argv) < 2:
+		print('usage: ' + sys.argv[0] + " 'percent of file to process' 'percent support threshold'")
+		exit()
+
+	percent_lines = float(sys.argv[1])
+	percent_support = float(sys.argv[2])
+	if percent_lines < 0 or percent_lines > 100 or percent_support < 0 or percent_support > 100:
+		print('percent must be between 0 and 100!')
+		exit()
+
 	retail = open('retail.txt')
-	threshold = 1000
 
 	num_lines = sum(1 for line in retail)
 	retail.seek(0)
-	lines_to_process = num_lines
+	lines_to_process = int((percent_lines * num_lines) // 100)
+	threshold = int((percent_support * lines_to_process) // 100)
 
-	# If a command line arg 'x' is given, we only read 'x'% of the lines in
-	# the 'retail.txt' file.
-	if len(sys.argv) > 1:
-		percent = float(sys.argv[1])
-		if percent < 0 or percent > 100:
-			print('percent must be between 0 and 100!')
-			exit()
-		lines_to_process = int((percent * num_lines) // 100)
+	print('Number of Baskets: ' + str(lines_to_process))
+	print('Support Threshold: ' + str(threshold))
+	print('Running apriori algorithm..')
 
-	# Run the apriori algorithm..
 	most_frequent_items = get_most_frequent_items(retail, lines_to_process, threshold)
 	item_pairs = list(itertools.combinations(most_frequent_items, 2))
 	most_frequent_pairs = get_most_frequent_pairs(retail, lines_to_process, item_pairs, threshold)
 
+	print('The most frequent pairs are:')
 	print(most_frequent_pairs)
 
 
