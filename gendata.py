674cb58008ae75f7fa7e64474cd45287aa2e7047
David Mulatti <david@mulatti.com>
2017-02-16 18:29:04 -0500

    Fancy new gendata.py

--- gendata.py
+++ gendata.py
@@ -1,14 +1,13 @@
 #!/usr/bin/env python3
 import random
+import os
 
 def main():
 	f = open('bigdata.txt', 'w')
-	for _ in range(200000):
-		s = ''
+	while os.path.getsize('bigdata.txt') < 9000000000:
 		for _ in range(random.randint(1, 20)):
-			s += str(random.randint(1, 1000)) + ' '
-		s += str(random.randint(1, 1000)) + '\n'
-		f.write(s)
+			f.write(str(random.randint(1, 1000)) + ' ')
+		f.write(str(random.randint(1, 1000)) + '\n')
 
 if __name__ == '__main__':
 	main()
