e3b706372d682deec6443ec77a029d86b31f8949
Michael Necio <michael@necio.ca>
2017-02-16 17:44:39 -0500

    Consted things

--- proj1.cpp
+++ proj1.cpp
@@ -13,7 +13,7 @@
 
 using namespace std;
 
-uint64_t get_current_memory_usage()
+inline uint64_t get_current_memory_usage()
 {
     struct task_basic_info t_info;
     mach_msg_type_number_t t_info_count = TASK_BASIC_INFO_COUNT;
@@ -21,7 +21,7 @@ uint64_t get_current_memory_usage()
     return t_info.resident_size;
 }
 
-inline uint32_t hash1(uint16_t first, uint16_t second)
+inline uint32_t hash1(const uint16_t first, const uint16_t second)
 {
     constexpr uint32_t FNV_prime = 16777619;
     constexpr uint32_t FNV_offset_basis = 2166136261;
@@ -37,7 +37,7 @@ inline uint32_t hash1(uint16_t first, uint16_t second)
     return hash;
 }
 
-inline uint32_t hash2(uint16_t first, uint16_t second)
+inline uint32_t hash2(const uint16_t first, const uint16_t second)
 {
     constexpr uint32_t FNV_prime = 16777619;
     constexpr uint32_t FNV_offset_basis = 2166136261;
@@ -58,7 +58,7 @@ inline uint32_t pair_16s_to_32(uint16_t a, uint16_t b)
     return ((uint32_t)a << 16) | b;
 }
 
-inline vector<bool> counts_to_frequent_bitmap(const vector<uint32_t> &counts, unsigned support_threshold)
+inline vector<bool> counts_to_frequent_bitmap(const vector<uint32_t> &counts, const unsigned support_threshold)
 {
     vector<bool> freq(counts.size(), false);
     for (unsigned i = 0; i < counts.size(); ++i)
@@ -71,7 +71,7 @@ inline vector<bool> counts_to_frequent_bitmap(const vector<uint32_t> &counts, un
     return freq;
 }
 
-void PCY_basic(const vector<vector<uint16_t>> &data, double support_percentage, double file_percentage)
+void PCY_basic(const vector<vector<uint16_t>> &data, const double support_percentage, const double file_percentage)
 {
     vector<uint32_t> item_counts(16500, 0);
     vector<uint32_t> pair_counts(data.size() * file_percentage * 4, 0);
@@ -86,15 +86,15 @@ void PCY_basic(const vector<vector<uint16_t>> &data, double support_percentage, 
 		{
 			for (unsigned j = i + 1; j < data[k].size(); ++j)
 			{
-			++pair_counts[hash1(data[k][i], data[k][j]) & pair_counts.size()];
+				++pair_counts[hash1(data[k][i], data[k][j]) & pair_counts.size()];
 			}
 		}
     }
 
     // The bool vector is implemented as a bitset
-    vector<bool> frequent_items = counts_to_frequent_bitmap(item_counts, data.size() * support_percentage);
+    const vector<bool> frequent_items = counts_to_frequent_bitmap(item_counts, data.size() * support_percentage);
     item_counts.resize(0); // deallocations memory used by vector
-    vector<bool> frequent_pairs = counts_to_frequent_bitmap(pair_counts, data.size() * support_percentage);
+    const vector<bool> frequent_pairs = counts_to_frequent_bitmap(pair_counts, data.size() * support_percentage);
     pair_counts.resize(0);
 
     unordered_map<uint32_t, uint32_t> candidate_pairs;
@@ -122,7 +122,58 @@ void PCY_basic(const vector<vector<uint16_t>> &data, double support_percentage, 
     }
 }
 
-void apriori(const vector<vector<uint16_t>> &data, double support_percentage, double file_percentage)
+void PCY_multihash(const vector<vector<uint16_t>> &data, const double support_percentage, const double file_percentage)
+{
+    vector<uint32_t> item_counts(16500, 0);
+    vector<uint32_t> pair_counts(data.size() * file_percentage * 4, 0);
+
+    for (unsigned k = 0; k < data.size() * file_percentage; ++k)
+    {
+		for (const auto &i : data[k])
+		{
+			++item_counts[i];
+		}
+		for (unsigned i = 0; i < data[k].size(); ++i)
+		{
+			for (unsigned j = i + 1; j < data[k].size(); ++j)
+			{
+			++pair_counts[hash1(data[k][i], data[k][j]) & pair_counts.size()];
+			}
+		}
+    }
+
+    // The bool vector is implemented as a bitset
+    const vector<bool> frequent_items = counts_to_frequent_bitmap(item_counts, data.size() * support_percentage);
+    item_counts.resize(0); // deallocations memory used by vector
+    const vector<bool> frequent_pairs = counts_to_frequent_bitmap(pair_counts, data.size() * support_percentage);
+    pair_counts.resize(0);
+
+    unordered_map<uint32_t, uint32_t> candidate_pairs;
+
+    for (unsigned k = 0; k < data.size() * file_percentage; ++k)
+    {
+		for (unsigned i = 0; i < data[k].size() && frequent_items[data[k][i]]; ++i)
+		{
+			for (unsigned j = i + 1; j < data[k].size(); ++j)
+			{
+				if (frequent_items[data[k][j]] && frequent_pairs[hash1(data[k][i], data[k][j]) & frequent_pairs.size()])
+				{
+					candidate_pairs[pair_16s_to_32(data[k][i], data[k][j])] += 1;
+				}
+			}
+		}
+    }
+
+    for (const auto &p : candidate_pairs)
+    {
+		if ((double)p.second / data.size() >= support_percentage)
+		{
+			cout << (p.first >> 16) << ' ' << (p.first & 0xffff) << '\n';
+		}
+    }
+}
+
+void apriori(const vector<vector<uint16_t>> &data, const double support_percentage, const double file_percentage)
 {
     unsigned baskets_to_process = file_percentage * data.size();
     unsigned threshold = support_percentage * baskets_to_process;
@@ -171,7 +222,7 @@ void apriori(const vector<vector<uint16_t>> &data, double support_percentage, do
     }
 }
 
-vector<vector<uint16_t>> read_baskets_from_file(string filename)
+vector<vector<uint16_t>> read_baskets_from_file(const string filename)
 {
     vector<vector<uint16_t>> data;
     string buf;
@@ -186,12 +237,12 @@ vector<vector<uint16_t>> read_baskets_from_file(string filename)
     return data;
 }
 
-int64_t benchmark(void (*func)(const vector<vector<uint16_t>> &, double, double),
-		  const vector<vector<uint16_t>> &data, double support, double file_percentage)
+inline uint64_t benchmark(void (*func)(const vector<vector<uint16_t>> &, const double, const double),
+		  const vector<vector<uint16_t>> &data, const double support, const double file_percentage)
 {
-    auto start = chrono::high_resolution_clock::now();
+    const auto start = chrono::high_resolution_clock::now();
     func(data, support, file_percentage);
-    auto end = chrono::high_resolution_clock::now();
+    const auto end = chrono::high_resolution_clock::now();
     return chrono::duration_cast<chrono::milliseconds>(end - start).count();
 }
 
